29c30543dd96e0dd2c8cd32db7c5869ade34124a
AKLukman <abulkashemlukman@gmail.com>
2022-02-01 13:56:37 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const { MongoClient, Admin } = require("mongodb");
@@ -13,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.json({ limit: "50mb" }));
+
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdvhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri)
@@ -30,8 +35,8 @@ async function run() {
     const sellerProfileCollection = database.collection("sellerProfile");
     const buyerProfileCollection = database.collection("buyerProfile");
     const commentsCollection = database.collection("comments");
-    // const reviewsCollection = database.collection('reviews');
-    // const ordersCollection = database.collection('orders');
+    const reviewsCollection = database.collection("reviews");
+    const scoresCollection = database.collection("scores");
 
     // Post Jobs
     app.post("/jobs", async (req, res) => {
@@ -99,7 +104,7 @@ async function run() {
       res.json(result);
     });
 
-    //Bid Job (By Md Hasib)----------------------------
+    //Bid Job ----------------------------
     app.post("/comments", async (req, res) => {
       const comments = req.body;
       const result = await commentsCollection.insertOne(comments);
@@ -137,11 +142,29 @@ async function run() {
       res.json(seller);
     });
 
+    // get using email
+    app.get("/sellerProfile", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = sellerProfileCollection.find(query);
+      const seller = await cursor.toArray();
+      res.json(seller);
+    });
     // get seller using id
     app.get("/sellerProfile/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await sellerProfileCollection.findOne(query);
+      res.json(result);
+    });
+
+    app.get("/seller/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await commentsCollection.findOne(query);
       res.json(result);
     });
 
@@ -240,20 +263,114 @@ async function run() {
       res.json(result);
     });
 
-    //Bid Job (By Md Hasib)----------------------------
+    //Bid Job----------------------------
     app.post("/comments", async (req, res) => {
       const comments = req.body;
       const result = await commentsCollection.insertOne(comments);
       res.json(result);
     });
 
-    // get Bid jobs and find by Id query (by Md Hasib)----------
+    // get Bid jobs----------
     app.get("/comments", async (req, res) => {
       const jobId = req.query.jobId;
       const query = { jobId: jobId };
       const cursor = commentsCollection.find(query);
       const comments = await cursor.toArray();
       res.json(comments);
+    });
+
+    // Reviews
+    app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewsCollection.insertOne(reviews);
+      res.json(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const sellerId = req.query.sellerId;
+      const query = { sellerId: sellerId };
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.json(reviews);
+    });
+
+    // Quiz scores
+    app.post("/scores", async (req, res) => {
+      const scores = req.body;
+      const result = await scoresCollection.insertOne(scores);
+      res.json(result);
+    });
+
+    app.get("/scores", async (req, res) => {
+      const cursor = scoresCollection.find({});
+      const score = await cursor.toArray();
+      res.json(score);
+    });
+
+    // NodeMailer
+
+    app.post("/api/form", (req, res) => {
+      let data = req.body;
+      console.log(data);
+      let smtpTransport = nodemailer.createTransport({
+        service: "Gmail",
+        port: 465,
+        auth: {
+          user: "mislura1998@gmail.com",
+          pass: "MISLU19982020",
+        },
+      });
+      const email = "abulkashemlukman@gmail.com";
+      const email1 = "mislu2081998@gmail.com";
+      let mailOptions = {
+        from: data.email,
+        to: [email, email1],
+        subject: `Message form ${data.email}`,
+        html: ` 
+      
+      <h3>Informations </h3>
+      <ul> 
+        <li>Name:${data.name}</li>
+        <li>Last Name:${data.lastname}</li>
+        <li>Email:${data.email}</li>
+      </ul>
+
+      <h3>Message</h3>     
+
+      <p>${data.message}</p>
+
+      `,
+      };
+      smtpTransport.sendMail(mailOptions, (error, response) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send("Success");
+        }
+      });
+      smtpTransport.close();
+    });
+
+    // Update status
+
+    app.put("/comments", async (req, res) => {
+      const { id, updateStatus } = req.body;
+      const query = { _id: ObjectId(id) };
+      const order = await commentsCollection.findOne(query);
+      if (order._id) {
+        const filter = { _id: ObjectId(order._id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            status: updateStatus,
+          },
+        };
+        const result = await commentsCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.json(result);
+      }
     });
   } finally {
     // await client.close();
